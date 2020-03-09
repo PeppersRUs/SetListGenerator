f72a820750b7422a85731dd3970466ec2eca7a83
PeppersRUs <simon.harte@live.co.uk>
2020-03-09 17:27:18 +0000

    refactoring

--- SetListGen.py
+++ SetListGen.py
@@ -4,30 +4,48 @@ import random
 rt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 setLength = None
 setList = []
+
+def lengthCheck(length):
+    while length > (rt.__len__()):
+        print("You do not have that many songs! Please try again.")
+        length = int(input("How many songs would you like to play?: "))
+    return
+
+def populateSet(length):
+    count = 0
+    while count < setLength:
+        # Generate random song from the repertoire
+        song = random.choice(rt)
+        # Check to make sure the song isnt alreaedy in the setlist
+        if setList.count(song) == 0:
+            setList.append(song)
+            count = count + 1
+        elif setList.count(song) > 0:
+            count = count
+    return
+
+def printSet(list):
+    for song in setList:
+        print(song)
+    return
+
 # Get the length
+
 setLength = int(input("How many songs would you like to play?: "))
 
 # Make sure the set length isn't longer than the repertoire
 
-while setLength > rt.__len__():
-    print("You do not have that many songs! Please try again.")
-    setLength = int(input("How many songs would you like to play?: "))
+lengthCheck(setLength)
 
 # Populate the list!
-count = 0
-while count < setLength:
-    # Generate random song from the repertoire
-    song = random.choice(rt)
-    # Check to make sure the song isnt alreaedy in the setlist
-    if setList.count(song) == 0:
-        setList.append(song)
-        count = count + 1
-    elif setList.count(song) > 0:
-        count = count
+
+populateSet(setLength)
 
 # Print out setlist
-for song in setList:
-    print(song)
+
+printSet(setList)
+
+
 
 input()
 
